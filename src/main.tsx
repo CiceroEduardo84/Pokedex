1dee97a3b0c06ba880c9054119af27889b94b2a3
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-01-03 21:16:54 -0300

    add base styled components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./pages/App";
+import { GlobalStyle } from "./styles/reset.ts";
+import { ThemeProvider } from "styled-components";
+import { appTheme } from "./styles/theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={appTheme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
+  </React.StrictMode>
+);
