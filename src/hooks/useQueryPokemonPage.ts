b5c31a3396513da13ca69091c671b03144756d06
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-02-05 21:24:57 -0300

    add effect home page

--- src/hooks/useQueryPokemonPage.ts
+++ src/hooks/useQueryPokemonPage.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { API } from "../configs/api";
 import { Pokemon } from "../@types/pokemon";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function useQueryPokemonPage() {
   const navigate = useNavigate();
+  const searchParams = useSearchParams();
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(30);
   const [totalPage, setTotalPages] = useState(1);
@@ -43,6 +44,11 @@ export function useQueryPokemonPage() {
 
     return pokemonData as Pokemon[];
   }
+
+  useEffect(() => {
+    const pageQuery = Number(searchParams[0].get("page"));
+    setPage(pageQuery || 1);
+  }, [searchParams]);
 
   const query = useQuery({
     queryKey: ["getPokemonPage", page, limit],
